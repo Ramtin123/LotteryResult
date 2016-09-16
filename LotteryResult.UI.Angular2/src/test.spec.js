c78beec263de01294a3cd1f6f1565850e6ffe039
Ramtin <ramtinab@yahoo.com>
2016-09-16 20:07:30 +1000

    .gitignore changing

--- LotteryResult.UI.Angular2/src/test.spec.js
+++
@@ -1,6 +0,0 @@
-import {MyList} from "./api.Service";
-describe("Hello world", function() {
-  it("says hello", function() {
-    expect("Hello world!").toEqual("Hello world!");
-  });
-});
--- LotteryResultViewer.WebApi/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="LotteryResultViewer.WebApi.WebApiApplication" Language="C#" %>
